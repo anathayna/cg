27509d233251fdda9512872a76fbf53026747ad5
anathayna <anathaynafranca@gmail.com>
2020-08-13 23:39:11 -0300

    adding splash project

---
+++ splash/splash/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  splash
+//
+//  Created by Ana Thayna Franca on 13/08/20.
+//  Copyright © 2020 Ana Thayna Franca. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
