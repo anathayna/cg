6261d44ff2467d04098c9d7466e21d378e9941dc
anathayna <anathaynafranca@gmail.com>
2020-09-24 22:24:41 -0300

    refactoring...

--- splash/splash/mesh.cpp
+++ splash/splash/mesh.cpp
@@ -18,21 +18,21 @@ void mesh::CreateMesh(GLfloat* vertices, unsigned int* indices, unsigned int num
     indexCount = numOfIndices;
 
     //Vertex Array Object: VAO
-    glGenVertexArrays(1, &VAO); //Gera um VAO ID
-    glBindVertexArray(VAO); //Atribui o ID ao VAO
+    glGenVertexArrays(1, &VAO); //gera um VAO ID
+    glBindVertexArray(VAO); //atribui o ID ao VAO
 
-        glGenBuffers(1, &IBO); //Gera um IBO ID
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO); //Atribui o ID ao IBO
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices[0]) * numOfIndices, indices, GL_STATIC_DRAW); //Atribuindo os valores dos indices para o IBO
+        glGenBuffers(1, &IBO); //gera um IBO ID
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO); //atribui o ID ao IBO
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices[0]) * numOfIndices, indices, GL_STATIC_DRAW); //atribuindo os valores dos indices para o IBO
 
-            //Carrega os dados do vértice para a memória da placa de vídeo
+            //carrega os dados do vértice para a memória da placa de vídeo
             //Vertex Buffer Object: VBO
-            glGenBuffers(1, &VBO); //Gera um VBO ID
-            glBindBuffer(GL_ARRAY_BUFFER, VBO); //Atribui o ID ao VBO
-            glBufferData(GL_ARRAY_BUFFER, sizeof(vertices[0]) * numOfVertices, vertices, GL_STATIC_DRAW); //Atribuindo os valores do vertice para o VBO
-                    //GL_STATIC_DRAW: Utilizado com desenhos que são fixos
-                    //GL_DYNAMIC_DRAW: Utilizado com desenhos que podem ter alterações durtante a apresentação
-                    //GL_STREAM_DRAW: Desenho é utilizado somente 1 frame
+            glGenBuffers(1, &VBO); //gera um VBO ID
+            glBindBuffer(GL_ARRAY_BUFFER, VBO); //atribui o ID ao VBO
+            glBufferData(GL_ARRAY_BUFFER, sizeof(vertices[0]) * numOfVertices, vertices, GL_STATIC_DRAW); //atribuindo os valores do vertice para o VBO
+                    //GL_STATIC_DRAW: utilizado com desenhos que são fixos
+                    //GL_DYNAMIC_DRAW: utilizado com desenhos que podem ter alterações durtante a apresentação
+                    //GL_STREAM_DRAW: desenho é utilizado somente 1 frame
 
                 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0); //0:  shader location | 3: numero de valores de vertice (x,y,z)
                                                                        //GL_FLOAT: Tipo de dados | GL_FALSE: normalizado | 0: pular elemento (cor)
@@ -45,11 +45,11 @@ void mesh::CreateMesh(GLfloat* vertices, unsigned int* indices, unsigned int num
 }
 
 void mesh::RenderMesh() {
-    glBindVertexArray(VAO); //Deixa na memória os links para serem utilizados
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO); //Para garantir que placas de vídeo antigas que não trabalham com Indices irão receber os indices
+    glBindVertexArray(VAO); //deixa na memória os links para serem utilizados
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO); //para garantir que placas de vídeo antigas que não trabalham com Indices irão receber os indices
             glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); //Remove o IBO da memória
-    glBindVertexArray(0); //Removo o VAO da memória
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); //remove o IBO da memória
+    glBindVertexArray(0); //removo o VAO da memória
 }
 
 mesh::~mesh() {
