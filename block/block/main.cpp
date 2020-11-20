7f39c953c20d059126ff0baef08a78cfcb1c9ce3
ana thayna <anathaynafranca@gmail.com>
2020-11-19 21:22:40 -0300

    start block project

---
+++ block/block/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  block
+//
+//  Created by ana thayna on 19/11/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
